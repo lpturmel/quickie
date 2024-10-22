fd263ec4afd2483c1ba71a5658325138573d0a77
Louis-Philippe Turmel <turmlo@2020spaces.com>
2024-10-22 12:05:44 -0400

    Added base64 decoding and rendering of images

--- app/src/components/response/Body.tsx
+++ app/src/components/response/Body.tsx
@@ -1,4 +1,4 @@
-import { Component, createEffect, createSignal } from "solid-js";
+import { Component, createEffect, createMemo, createSignal, Show } from "solid-js";
 import { Response } from "../../utils/api";
 import { codeToHtml } from "shiki";
 
@@ -7,23 +7,32 @@ interface BodyProps {
 }
 const Body: Component<BodyProps> = (props) => {
     const [hydrated, setHydrated] = createSignal("");
-    const contentType = props.res.headers["content-type"];
-    const lang = () => {
-        if (contentType?.includes("application/json")) {
+    const contentType = createMemo(() => props.res.headers["content-type"] || "text/plain");
+    const lang = createMemo(() => {
+        if (contentType().includes("application/json")) {
             return "json"
-        } else if (contentType?.includes("text/html")) {
+        } else if (contentType().includes("text/html")) {
             return "html"
-        } else if (contentType?.includes("application/xml")) {
+        } else if (contentType().includes("application/xml")) {
             return "xml"
-        } else {
+        } else if (contentType() === "text/plain") {
             return "plaintext"
-        };
-    }
+        } else {
+            return "image"
+        }
+    });
 
-    const body = () => props.res.body;
+    // Data from the app backend is base64 encoded
+    const body = createMemo(() => props.res.body);
+
+    const imageUrl = createMemo(() => `data:${contentType()};base64,${body()}`);
 
     createEffect(async () => {
-        const html = await codeToHtml(body(), {
+        console.log(contentType());
+        console.log(body().length);
+        console.log(lang());
+        if (lang() == "image") return;
+        const html = await codeToHtml(atob(body()), {
             lang: lang(), theme: "everforest-dark", colorReplacements: {
                 "#2d353b": "transparent",
             }
@@ -32,14 +41,18 @@ const Body: Component<BodyProps> = (props) => {
 
     });
 
-    if (!contentType?.includes("image")) {
-        return (
-            <div class="code-container text-sm">
-                <div innerHTML={hydrated()} />
-            </div>
-        )
-    }
-    return <img src={props.res.body} />;
+    return (
+        <>
+            <Show when={contentType().startsWith("image/")}>
+                <img src={imageUrl()} alt="Response Image" />
+            </Show>
+            <Show when={!contentType().startsWith("image/")}>
+                <div class="code-container text-sm">
+                    <div innerHTML={hydrated()} />
+                </div>
+            </Show>
+        </>
+    )
 }
 
 export default Body;
